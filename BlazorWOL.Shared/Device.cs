f14bec8441dce9284b38729a61b47274fb64cba1
thara0402 <thara0402@gmail.com>
2019-07-07 20:01:38 +0900

    add project.

---
+++ BlazorWOL.Shared/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorWOL.Shared
+{
+    public class Device
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; }
+
+        public string MACAddress { get; set; }
+    }
+}
